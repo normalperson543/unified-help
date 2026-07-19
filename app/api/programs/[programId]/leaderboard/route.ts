0d1b56ce75cf2c57651d6c82fd94d921b8ef54b3
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-07-19 01:43:18 -0700

    oop track route.ts leaderboard from earlier commit

---
+++ app/api/programs/[programId]/leaderboard/route.ts
@@ -0,0 +1,40 @@
+import {
+  getResolvedTicketsCount,
+  getUserAuthStatus,
+} from "@/app/lib/data";
+
+import { type NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  ctx: RouteContext<"/api/programs/[programId]/leaderboard">,
+) {
+  const { programId } = await ctx.params;
+  const authStatus = await getUserAuthStatus();
+  if (authStatus.status === "unauthenticated") {
+    return new Response(JSON.stringify({ status: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+  
+  const params = req.nextUrl.searchParams;
+  console.log(params);
+  const oldest = params.get("oldest");
+  const newest = params.get("newest");
+
+  let oldestDate;
+  if (oldest) {
+    oldestDate = new Date(Number(oldest));
+  } else {
+    oldestDate = new Date("01-01-2000") // idk vro
+  }
+  let newestDate;
+  if (newest) {
+    newestDate = new Date(Number(newest));
+  } else {
+    newestDate = new Date()
+  }
+  
+  const program = await getResolvedTicketsCount(programId, oldestDate, newestDate);
+  return new Response(JSON.stringify(program));
+}
