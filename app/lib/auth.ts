5d72e83efbbf75fad59564e78ff97b1dcb0dad91
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-07-22 23:32:33 -0400

    fix state mismatch by pointing baseURL of auth to the env var instead

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -10,7 +10,7 @@ import { createUser } from "./slack";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env["BETTER_AUTH_URL"],
   cookieCache: {
     enabled: true,
     maxAge: 5 * 60,
