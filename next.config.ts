603b4755ec6cbc6eeb0b89ab66dcf8d002884b38
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-07-30 15:31:11 -0700

    why did sentry do that

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "sidewayscoder",
+  org: process.env.SENTRY_PROJECT_ORG,
 
-  project: "unified-help",
+  project: process.env.SENTRY_PROJECT_NAME,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
