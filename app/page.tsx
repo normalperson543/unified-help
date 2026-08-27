585a4f0a406ff784cce117601dda0707675eed21
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-08-26 23:25:44 -0700

    fix pages not building properly in orcahrd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { connection } from "next/server";
 import { getAllStats, getPrograms } from "./lib/data";
 import HomeUI from "./ui/home";
 
 export default async function Home() {
+  await connection();
   const stats = await getAllStats();
   const programs = await getPrograms();
 
